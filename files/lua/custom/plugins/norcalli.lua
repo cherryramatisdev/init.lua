14a4b913eb33eff6a0c450c511b8b13dab995380
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-01-25 14:45:38 -0300

    Plugins for all norcalli

---
+++ files/lua/custom/plugins/norcalli.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"norcalli/nvim-colorizer.lua",
+    branch = 'color-editor',
+		config = function()
+			require("colorizer").setup({
+				"*",
+			})
+		end,
+	},
+	{
+		"norcalli/nvim-terminal.lua",
+		config = function()
+			require("terminal").setup()
+		end,
+	},
+	{ "norcalli/nvim-base16.lua", branch = "theme-editor" },
+  "norcalli/nvim-popterm.lua",
+}
