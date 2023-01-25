cc1b90c317a717e910748949268bf3bd61b6448b
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-01-25 14:45:49 -0300

    Add more plugins

--- files/lua/custom/plugins/init.lua
+++ files/lua/custom/plugins/init.lua
@@ -3,21 +3,28 @@ return {
   "antoinemadec/FixCursorHold.nvim",
   "tjdevries/colorbuddy.vim",
   "tjdevries/gruvbuddy.nvim",
+  "Mofiqul/vscode.nvim",
   "kyazdani42/nvim-web-devicons",
   "kdheepak/lazygit.nvim",
   "stevearc/oil.nvim",
   "tpope/vim-fugitive",
   "tpope/vim-rhubarb",
+  "tpope/vim-rsi",
+  {
+    "epwalsh/obsidian.nvim",
+    config = function()
+      require("obsidian").setup({
+        dir = "/Users/cherryramatis/Library/Mobile Documents/iCloud~md~obsidian/Documents/Brain",
+        completion = {
+          nvim_cmp = true,
+        },
+      })
+    end,
+  },
   {
     "andweeb/presence.nvim",
     config = function()
       require("presence"):setup()
-    end,
-  },
-  {
-    "beauwilliams/focus.nvim",
-    config = function()
-      require("focus").setup()
     end,
   },
 }
