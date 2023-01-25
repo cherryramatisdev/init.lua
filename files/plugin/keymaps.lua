f96c5eb3adc2c088e5faa8e1a19c7c5b529ee3eb
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-01-25 14:45:27 -0300

    Add some keybindings

--- files/plugin/keymaps.lua
+++ files/plugin/keymaps.lua
@@ -6,8 +6,25 @@ vim.keymap.set("n", "<leader>g", ":LazyGit<cr>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set("n", "<C-c>", function()
+	require("colorizer").color_picker_on_cursor()
+end)
+
+vim.keymap.set('n', '<leader>s', [[:s/\w\+/\=v:lua.string.format('%s', submatch(0))/g]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set("n", "<right>", [[<C-w><]])
+vim.keymap.set("n", "<left>", [[<C-w>>]])
+vim.keymap.set("n", "<up>", [[<C-w>+]])
+vim.keymap.set("n", "<down>", [[<C-w>-]])
+
+for i in pairs({ 1, 2, 3, 4, 5 }) do
+	vim.keymap.set("n", "<leader>" .. i, function()
+		POPTERM(i)
+	end)
+end
