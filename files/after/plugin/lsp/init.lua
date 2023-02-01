2def54e812c6560087198f10708bb0fe783ef809
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-01-31 21:43:51 -0300

    Use autocmd to add mappings

--- files/after/plugin/lsp/init.lua
+++ files/after/plugin/lsp/init.lua
@@ -8,36 +8,45 @@ local navic = require("nvim-navic")
 
 local lspconfig = require("lspconfig")
 
-local on_attach = function(client, bufnr)
-	navic.attach(client, bufnr)
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local bufnr = args.buf
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
 
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-	-- Enable go to definition triggered by <c-]>
-	vim.api.nvim_buf_set_option(bufnr, "tagfunc", "v:lua.vim.lsp.tagfunc")
+		if client.name ~= "null-ls" then
+			navic.attach(client, bufnr)
+		end
 
-	local opts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-	vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-	vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-	vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-	vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-	vim.keymap.set("n", "ga", vim.lsp.buf.code_action, opts)
-	vim.keymap.set("n", "gR", vim.lsp.buf.references, opts)
-	vim.keymap.set("n", "rn", vim.lsp.buf.rename, opts)
-	vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, opts)
-	vim.keymap.set("n", "<leader>f", function()
-		vim.lsp.buf.format({ async = true })
-	end, opts)
-end
+		if client.server_capabilities.completionProvider then
+			vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+		end
+		if client.server_capabilities.definitionProvider then
+			vim.bo[bufnr].tagfunc = "v:lua.vim.lsp.tagfunc"
+		end
 
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	group = vim.api.nvim_create_augroup("phplsp", {}),
-	pattern = "*.php",
-	callback = function()
-		on_attach(nil, vim.api.nvim_get_current_buf())
+		local opts = { noremap = true, silent = true, buffer = bufnr }
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+		vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+		vim.keymap.set("n", "ga", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "gR", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, opts)
+		vim.keymap.set("n", "<leader>f", function()
+			vim.lsp.buf.format({ async = true })
+		end, opts)
 	end,
 })
+
+-- vim.api.nvim_create_autocmd({ "BufEnter" }, {
+-- 	group = vim.api.nvim_create_augroup("phplsp", {}),
+-- 	pattern = "*.php",
+-- 	callback = function()
+-- 		on_attach(nil, vim.api.nvim_get_current_buf())
+-- 	end,
+-- })
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
