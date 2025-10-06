0421ff792f3904a4b62586629d172c1b1d880fe1
Cherry Ramatis <cherry.ramatis@icloud.com>
2025-10-06 08:35:45 -0300

    Allow to remove entries from quickfix list

--- plugin/qf.lua
+++ plugin/qf.lua
@@ -8,3 +8,28 @@ vim.api.nvim_create_autocmd({ "QuickFixCmdPost" }, {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = "qf",
+  callback = function()
+    vim.keymap.set("n", "dd", function()
+      local idx = vim.fn.line "."
+      local qflist = vim.fn.getqflist()
+      table.remove(qflist, idx)
+      vim.fn.setqflist(qflist, "r")
+    end, { buffer = true })
+
+    vim.keymap.set("v", "d", function()
+      local start_line = vim.fn.line "v"
+      local end_line = vim.fn.line "."
+      local qflist = vim.fn.getqflist()
+
+      for i = math.max(end_line, start_line), math.min(end_line, start_line), -1 do
+        table.remove(qflist, i)
+      end
+
+      vim.fn.setqflist(qflist, "r")
+      vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, true, true), "v")
+    end, { buffer = true })
+  end,
+})
