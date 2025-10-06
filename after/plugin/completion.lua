5360d4649cf2f482c08d37daee9249064a91e4c8
Cherry Ramatis <cherry.ramatis@icloud.com>
2025-10-06 18:13:23 -0300

    Disable cmdline completion

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -14,6 +14,7 @@ end
 require("blink.cmp").setup {
   keymap = { preset = "default" },
   appearance = { nerd_font_variant = "mono" },
+  cmdline = { enabled = false },
   completion = { documentation = { auto_show = true } },
   sources = {
     default = { "lsp", "path", "snippets", "buffer" },
