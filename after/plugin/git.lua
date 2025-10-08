21602d64c437d8f99bd4331fbb07e710d163a8f7
Cherry Ramatis <cherry.ramatis@icloud.com>
2025-10-08 08:14:43 -0300

    Load CTRLG on startup

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -1,7 +1,4 @@
 vim.pack.add({
   { src = "https://github.com/rhysd/committia.vim" },
-}, { load = true })
-
-vim.pack.add {
   { src = "https://github.com/cherryramatisdev/CTRLGGitBlame.vim" },
-}
+}, { load = true })
