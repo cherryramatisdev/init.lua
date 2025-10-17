9fe728263eb4209f56b110253432b7c2d765ecdb
Cherry Ramatis <cherry.ramatis@icloud.com>
2025-10-17 18:39:04 -0300

    Make formatting silent

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "c<cr>", function()
-  vim.cmd("! " .. vim.fn.stdpath "data" .. "/mason/bin/stylua " .. vim.fn.expand "%")
+  vim.cmd("silent ! " .. vim.fn.stdpath "data" .. "/mason/bin/stylua " .. vim.fn.expand "%")
 end, { buffer = true })
