271b0359a56614d325d5e3d1199de11693e22af3
Cherry Ramatis <cherry.ramatis@icloud.com>
2025-10-17 18:38:52 -0300

    Add markdown plugins

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -25,3 +25,28 @@ require("img-clip").setup()
 vim.keymap.set("n", "cp", function()
   require("img-clip").paste_image()
 end, { buffer = true, desc = "Paste image from clipboard" })
+
+vim.pack.add({
+  { src = "https://github.com/nvim-mini/mini.icons" },
+  { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
+}, { load = true })
+
+if not pcall(require, "render-markdown") then
+  return
+end
+
+require("render-markdown").setup {
+  enabled = true,
+  render_modes = { "n", "c", "t", "i" },
+  completions = { blink = { enabled = true } },
+}
+
+vim.pack.add {
+  "https://github.com/SCJangra/table-nvim",
+}
+
+if not pcall(require, "table-nvim") then
+  return
+end
+
+require("table-nvim").setup {}
