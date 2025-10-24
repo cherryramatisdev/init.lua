42ccf2d9fbd60471ec74341771e13482729a20a6
Cherry Ramatis <cherry.ramatis@icloud.com>
2025-10-24 18:17:59 -0300

    Add plugin to deal with git conflict

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -2,3 +2,13 @@ vim.pack.add({
   { src = "https://github.com/rhysd/committia.vim" },
   { src = "https://github.com/cherryramatisdev/CTRLGGitBlame.vim" },
 }, { load = true })
+
+vim.pack.add {
+  "https://github.com/akinsho/git-conflict.nvim",
+}
+
+if not pcall(require, "git-conflict") then
+  return
+end
+
+require("git-conflict").setup {}
